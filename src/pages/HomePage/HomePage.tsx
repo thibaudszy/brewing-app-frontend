e82819e04e69e980c2aaf0086206486e4e10a05b
thibaudszy <thivaudszy@gmail.com>
2020-11-29 17:34:15 +0100

    Changes messages on the homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { selectToken, selectUserLanguage } from "../../store/user/selectors";
+import { useSelector } from "react-redux";
+import { Button } from "react-bootstrap";
+import translation from "./translation";
 
 export default function HomePage() {
-  return <div>Sign up to start brewing</div>;
+  const userLanguage: Language = useSelector(selectUserLanguage);
+  const { t_not_logged_in_message } = translation[userLanguage];
+  const token = useSelector(selectToken);
+  if (!token) {
+    return (
+      <div>
+        <h1> {t_not_logged_in_message}</h1>
+      </div>
+    );
+  }
+  return <h1> Let's get brewing </h1>;
 }
---
+++ src/pages/HomePage/translation.ts
@@ -0,0 +1,11 @@
+const translation: TranslationObject = {
+  "En-GB": {
+    t_not_logged_in_message: "Sign-up or log in to start brewing",
+  },
+  "Fr-FR": {
+    t_not_logged_in_message:
+      "Creez un compte ou connectez-vous pour commencer a brasser",
+  },
+};
+
+export default translation;
