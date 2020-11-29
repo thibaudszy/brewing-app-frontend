fd0e26190558a0f71c315d8976b4121804019c22
thibaudszy <thivaudszy@gmail.com>
2020-11-29 16:18:42 +0100

    :sparkles: user can remove recipes from library

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Switch from "react-bootstrap/esm/Switch";
-import { Route } from "react-router-dom";
+import { Route, useHistory } from "react-router-dom";
 import "./App.css";
 import AppNavbar from "./components/Navbar/AppNavbar";
 import Login from "./pages/Login/Login";
@@ -11,8 +11,15 @@ import HomePage from "./pages/HomePage/HomePage";
 import MyRecipes from "./pages/MyRecipes/MyRecipes";
 import RecipePage from "./pages/RecipePage/RecipePage";
 import ImportRecipes from "./pages/ImportRecipes/ImportRecipes";
+import { selectToken, selectUserId } from "./store/user/selectors";
+import { useSelector } from "react-redux";
 
 function App() {
+  const token = useSelector(selectToken);
+  const history = useHistory();
+  const userId = useSelector(selectUserId);
+  //TO DO: if no token -> homePage
+  //TO DO: if no userId, send request to api/me
   return (
     <div className="App">
       <AppNavbar />
